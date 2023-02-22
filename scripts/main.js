7b139865b6ed167e58ff6fb4b37bdeab8fed267d
Camilo Rincon <crincon@uniempresarial.edu.co>
2023-02-22 14:50:42 -0500

    Feb-22

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,25 @@
 'use strict';
 
 const btnDescript = document.getElementById('descripccion');
-btnDescript.addEventListener('click', ()=>{
+/*btnDescript.addEventListener('click', ()=>{
     alert("Hola buenas tardes");
 });
+*/
+const $form = document.getElementById('form');
+$form.addEventListener('submit', handlesubmit);
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form, 
+        headers: {
+            'Accept': 'application/json'        
+        }
+    })
+    if (response.ok){
+        this.reset();
+        alert('Muchas tenkius por tus datos, te robaremos la cuenta');
+    }
+}
