280af10fbf84f95fb1b39356853e0d367506a1b5
kevinospina142 <kh5564765@gmail.com>
2023-02-23 22:59:58 -0500

    23/feb

--- scripts/main.js
+++ scripts/main.js
@@ -19,3 +19,13 @@ document.addEventListener("change", e=>{
         })
     }
 })
+
+// Funciones para almacenar
+function guardarAlmacenamientoLocal(llave, valor_a_guardar) {
+    localStorage.setItem(llave, JSON.stringify(valor_a_guardar))
+}
+function obtenerAlmacenamientoLocal(llave) {
+    const datos = JSON.parse(localStorage.getItem(llave))
+    return datos
+}
+let productos = obtenerAlmacenamientoLocal('productos') || [];
