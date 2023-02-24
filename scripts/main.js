0c3cc761457700359ba76873399239f2e6d8c3d1
Kevinospina142 <115588112+Kevinospina142@users.noreply.github.com>
2023-02-24 10:51:10 -0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -6,9 +6,9 @@ document.addEventListener("keyup", e=>{
             producto.textContent.toLowerCase().includes(e.target.value.toLowerCase())
                 ?producto.classList.remove("filt")
                 :producto.classList.add("filt")
-        })
+        });
     }
-})
+});
 
 document.addEventListener("change", e=>{
     if (e.target.matches("#buscador")){
@@ -16,16 +16,32 @@ document.addEventListener("change", e=>{
             producto.textContent.toLowerCase().includes(e.target.value.toLowerCase())
                 ?producto.classList.remove("filt")
                 :producto.classList.add("filt")
-        })
+        });
     }
-})
+});
 
-// Funciones para almacenar
-function guardarAlmacenamientoLocal(llave, valor_a_guardar) {
-    localStorage.setItem(llave, JSON.stringify(valor_a_guardar))
-}
-function obtenerAlmacenamientoLocal(llave) {
-    const datos = JSON.parse(localStorage.getItem(llave))
-    return datos
-}
-let productos = obtenerAlmacenamientoLocal('productos') || [];
+const descrip1 = document.getElementById("descripcion");
+const descrip2 = document.getElementById("descripcion2"); 
+const descrip3 = document.getElementById("descripcion3");
+const descrip4 = document.getElementById("descripcion4");
+const descrip5 = document.getElementById("descripcion5");
+const descrip6 = document.getElementById("descripcion6"); 
+
+descrip1.addEventListener("click",()=>{
+    alert("Ak-47, plateado, común");
+});
+descrip2.addEventListener("click",()=>{
+    alert("desert eagle, rojo, épico");
+});
+descrip3.addEventListener("click",()=>{
+    alert("M4A1, celeste con negro, legendario");
+});
+descrip4.addEventListener("click",()=>{
+    alert("MP5, rojo, poco común");
+});
+descrip5.addEventListener("click",()=>{
+    alert("1000 pavos de fortnite");
+});
+descrip6.addEventListener("click",()=>{
+    alert("M4A1, azul con morado, legendario");
+});
